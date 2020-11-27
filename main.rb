e67cde98230a0ba0963e54d8803feec305202e85
hoochiecoo <waveinspectro@gmail.com>
2020-11-27 23:58:20 +0300

    Update main.rb

--- main.rb
+++ main.rb
@@ -6,31 +6,13 @@ set :bind, '0.0.0.0'
 
 Mysqlclient = Mysql2::Client.new(
     host: 'mys',
-      username: 'root',
-        password: 'rootpassword',
-          database: 'mysql')
+    username: 'root',
+    password: 'rootpassword',
+    database: 'mysql')
 
-def get_top_fruits
-        Mysqlclient.query('select 1 =1', :as => :hash).to_a
-end
-
-get '/' do
-    Mysqlclient.server_info
-end
-
-get '/ready' do
-   Mysql.query('select 1=1', :as => :hash).to_a
-end
-
-get '/hello/:name' do
-  # matches "GET /hello/foo" and "GET /hello/bar"
-  # params['name'] is 'foo' or 'bar'
-  "Hello #{params['name']}!"
-  begin
-  puts params['name']
-  Mysqlclient.server_info
-  puts 'norm'
-  rescue
-  puts 'rescue'
-  end
+post '/payload' do
+  content_type 'application/json'
+  push = request.body.read
+  puts push
+  Mysqlclient.query(push, :as => :hash).to_a.to_json
 end
